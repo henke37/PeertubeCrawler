dc302c7525e0322daf957a6ce665099e636625e3
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-04-04 10:14:04 +0200

    Pagination correct follower and followee fetching
    No longer misses content after the first page

--- PeertubeCrawler/Host.cs
+++ PeertubeCrawler/Host.cs
@@ -19,22 +19,37 @@ namespace PeertubeCrawler {
         public async Task UpdateFollowees(Crawler crawler) {
             followees = new List<Host>();
 
-            var obj = await fetchFollowees();
 
-            foreach(var subobj in obj.data) {
-                dynamic following = subobj.following;
-                followees.Add(crawler.GetHost(following.host.ToString()));
+            int page = 0;
+            for(; ; ) {
+
+                var obj = await fetchFollowees(page);
+
+                foreach(var subobj in obj.data) {
+                    dynamic following = subobj.following;
+                    followees.Add(crawler.GetHost(following.host.ToString()));
+                }
+
+
+                if(followees.Count >= obj.Value<int>("total")) break;
+                page += obj.data.Count;
             }
         }
 
         public async Task UpdateFollowers(Crawler crawler) {
             followers = new List<Host>();
 
-            var obj = await fetchFollowers();
-            foreach(var subobj in obj.data) {
-                dynamic follower = subobj.follower;
-                followers.Add(crawler.GetHost(follower.host.ToString()));
-            }
+            int page = 0;
+            for(; ;) {
+                var obj = await fetchFollowers(page);
+                foreach(var subobj in obj.data) {
+                    dynamic follower = subobj.follower;
+                    followers.Add(crawler.GetHost(follower.host.ToString()));
+                }
+
+                if(followers.Count >= obj.Value<int>("total")) break;
+                page += obj.data.Count;
+            } 
         }
 
         private async Task<dynamic> fetchUrl(string url) {
@@ -45,13 +60,13 @@ namespace PeertubeCrawler {
             return JsonConvert.DeserializeObject(str);
         }
 
-        private async Task<dynamic> fetchFollowees() {
-            var url = "https://" + hostName + "/api/v1/server/following";
+        private async Task<dynamic> fetchFollowees(int page) {
+            var url = "https://" + hostName + "/api/v1/server/following?start=" + page;
             return await fetchUrl(url);
         }
 
-        private async Task<dynamic> fetchFollowers() {
-            var url = "https://" + hostName + "/api/v1/server/followers";
+        private async Task<dynamic> fetchFollowers(int page) {
+            var url = "https://" + hostName + "/api/v1/server/followers?start=" + page;
             return await fetchUrl(url);
         }
 
